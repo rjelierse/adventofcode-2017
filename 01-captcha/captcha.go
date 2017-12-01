1b41ab5edbc86c5848753942af513baa1ddc86a3
Raymond Jelierse <info@raymondjelierse.nl>
2017-12-01 10:00:04 +0100

    Create adjustable offset for determining matching pairs
    Solves puzzle for day 1

--- 01-captcha/captcha.go
+++ 01-captcha/captcha.go
@@ -11,26 +11,11 @@ import (
 
 func main() {
 	input := strings.Split(readInput(os.Args[1]), "")
-	sum := int64(0)
-	input = append(input, input[0])
+	// Part 1:
+	fmt.Println("[1] Captcha result:", calculateSum(input, 1))
 
-	for i := 0; i < (len(input) - 1); i++ {
-		current, err := strconv.ParseInt(input[i], 10, 0)
-		if err != nil {
-			log.Fatal("Cannot parse number:", err)
-		}
-
-		next, err := strconv.ParseInt(input[i+1], 10, 0)
-		if err != nil {
-			log.Fatal("Cannot parse number:", err)
-		}
-
-		if current == next {
-			sum = sum + current
-		}
-	}
-
-	fmt.Println("Sum of adjacent numbers:", sum)
+	// Part 2:
+	fmt.Println("[2] Captcha result:", calculateSum(input, len(input) / 2))
 }
 
 func readInput(path string) string {
@@ -40,4 +25,27 @@ func readInput(path string) string {
 	}
 
 	return string(bytes[:])
+}
+
+func calculateSum(input []string, offset int) (sum int64) {
+	length := len(input)
+
+	for i := 0; i < length; i++ {
+		j := (i + offset) % length
+		current, err := strconv.ParseInt(input[i], 10, 0)
+		if err != nil {
+			log.Fatal("Cannot parse number:", err)
+		}
+
+		next, err := strconv.ParseInt(input[j], 10, 0)
+		if err != nil {
+			log.Fatal("Cannot parse number:", err)
+		}
+
+		if current == next {
+			sum = sum + current
+		}
+	}
+
+	return sum
 }
