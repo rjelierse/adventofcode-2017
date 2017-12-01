43aba49b3e85e7a2f9542ca1ec0a9e5c20d51663
Raymond Jelierse <info@raymondjelierse.nl>
2017-12-01 13:51:45 +0100

    Optimize calculations by removing type conversions

--- 01-captcha/captcha.go
+++ 01-captcha/captcha.go
@@ -1,49 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"strings"
-	"strconv"
-	"fmt"
 )
 
+const AsciiOffset = 48
+
 func main() {
-	input := strings.Split(readInput(os.Args[1]), "")
+	input := readInput(os.Args[1])
 	// Part 1:
 	fmt.Println("[1] Captcha result:", calculateSum(input, 1))
 
 	// Part 2:
-	fmt.Println("[2] Captcha result:", calculateSum(input, len(input) / 2))
+	fmt.Println("[2] Captcha result:", calculateSum(input, len(input)/2))
 }
 
-func readInput(path string) string {
+func readInput(path string) []byte {
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
 		log.Fatal("Cannot read file:", err)
 	}
 
-	return string(bytes[:])
+	return bytes
 }
 
-func calculateSum(input []string, offset int) (sum int64) {
+func calculateSum(input []byte, offset int) (sum int) {
 	length := len(input)
 
 	for i := 0; i < length; i++ {
 		j := (i + offset) % length
-		current, err := strconv.ParseInt(input[i], 10, 0)
-		if err != nil {
-			log.Fatal("Cannot parse number:", err)
-		}
 
-		next, err := strconv.ParseInt(input[j], 10, 0)
-		if err != nil {
-			log.Fatal("Cannot parse number:", err)
-		}
+		a := int(input[i]) - AsciiOffset
+		b := int(input[j]) - AsciiOffset
 
-		if current == next {
-			sum = sum + current
+		if a == b {
+			sum = sum + a
 		}
 	}
 
